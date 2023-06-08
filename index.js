b1c69a38013e8025000a688591b6170c5b8119e3
Sheshant-Manure <msheshant1997@gmail.com>
2023-06-08 22:02:29 +0530

    Setting Routes folder for handling url requests

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Importing the required modules, libraries and frameworks
 const express = require('express');
 const app = express();
- 
+
+// Setting Routes folder for routing all the urls requested by the client (browser)
+app.use('/', require('./Routes/index'));
+
 // Defining the localhost url and port #
 const url = '127.0.0.1';
 const port = 8000;
