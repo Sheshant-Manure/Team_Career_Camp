133880205dbaf3f3f81000e662cddb46a4a4c4f8
Sheshant-Manure <msheshant1997@gmail.com>
2023-06-09 01:18:37 +0530

    Installing express-ejs-layouts. Setup Partials folder for header and footer.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Importing the required modules, libraries and frameworks
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 // Setting 'Assets' folder as directory for static files (CSS, JS and Images)
@@ -8,6 +9,14 @@ app.use(express.static('./Assets'));
 // Setting up the EJS view engine 
 app.set('view engine', 'ejs');
 app.set('views', './Views'); // The http requests will be routed to the 'Views' directory wherein all the pages and subpages are stored
+
+// Extract the styles and scripts from subpages into the layout page
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// Using express ejs layouts library
+// This must be executed before the routes so that before rendering pages from the views, a layout is predefined for all of them
+app.use(expressLayouts);
 
 // Setting Routes folder for routing all the urls requested by the client (browser)
 app.use('/', require('./Routes/index'));
