480ea5c695254a7e66b0342e9a8b3f948a9a2d75
Sheshant-Manure <msheshant1997@gmail.com>
2023-06-22 01:25:00 +0530

    changed app.listen()

--- index.js
+++ index.js
@@ -40,14 +40,11 @@ app.use(session({
 // Setting Routes folder for routing all the urls requested by the client (browser)
 app.use('/', require('./Routes/index'));
 
-// Defining the localhost url and port #
-const url = '127.0.0.1';
+// Defining the port #
 const port = 8000;
 
 // Setting up the express server at localhost(127.0.0.1 and port:8000)
-app.listen(port, url, (error)=>{
-    if(error){
-        console.log(error);
-    }
-    console.log(`Server is running at ${url}:${port}`);
-});
+// Running the server at defined port
+app.listen(port, function() {
+    console.log('Local server running at http://127.0.0.1:', port);
+  })
